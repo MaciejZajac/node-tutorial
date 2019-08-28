b93f9c8770815bdf0a3b1aeb28a7795721479fe0
Maciej Zając <maciej.zajac.197@gmail.com>
2019-08-28 22:48:50 +0200

    express routes

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware!");
+app.use("/add-product", (req, res, next) => {
+    res.send("<h1>Add product</h1>");
 
     // allows the request to continue to the next middleware
-    next();
 });
 
-app.use((req, res, next) => {
-    console.log("In the middleware!2");
+app.use("/", (req, res, next) => {
+    res.send("<h1>Hello from express</h1>");
 });
 
 const server = http.createServer(app);
