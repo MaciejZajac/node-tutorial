8f6000fedf748b5a6388a450a01f593e24014711
Maciej Zając <maciej.zajac.197@gmail.com>
2019-08-29 20:50:59 +0200

    express styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require("path");
+const rootDir = require("../util/path");
 const router = express.Router();
 
 router.get("/add-product", (req, res) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+    res.sendFile(path.join(rootDir, "views", "add-product.html"));
     // res.send(
     //     "<form action='/admin/product' method='POST'><input type='text' name='title'></input><button type='submit'>Send</button></form>"
     // );
