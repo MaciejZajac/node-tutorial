bef94ff46674c0da88137564e230a69d96342728
Maciej Zając <maciej.zajac.197@gmail.com>
2019-08-31 14:36:31 +0200

    task

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const express = require("express");
-const path = require("path");
-const bodyparser = require("body-parser");
+const express = require('express');
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const users = [];
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-app.use(bodyparser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.render("users", { pageTitle: "List of users", users: users })
+})
+app.get("/add-user", (req, res) => {
+    res.render("add-user", { pageTitle: "Add User" })
+})
 
-app.use("/admin", adminData.router);
-app.use(shopRoutes);
+app.post("/add-user", (req, res) => {
+    users.push({ name: req.body.user });
+    res.redirect("/users");
+})
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-    res.status(404).render("404", { docTitle: "404 - not Found" });
-});
 
 app.listen(3000);
