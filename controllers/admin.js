db8fc591891b50b576159d02035c07ed7499e207
Maciej Zając <maciej.zajac.197@gmail.com>
2019-10-08 22:27:37 +0200

    different way of working with endpoints

--- controllers/admin.js
+++ controllers/admin.js
@@ -152,8 +152,8 @@ exports.postEditProduct = (req, res) => {
         });
 };
 
-exports.postDeleteProduct = (req, res) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res) => {
+    const prodId = req.params.productId;
     Product.findById(prodId)
         .then(product => {
             if (!product) {
@@ -163,12 +163,18 @@ exports.postDeleteProduct = (req, res) => {
             return Product.deleteOne({ _id: prodId, userId: req.user._id });
         })
         .then(() => {
-            res.redirect("/admin/products");
+            // res.redirect("/admin/products");
+            res.status(200).json({
+                message: "Success!"
+            });
         })
         .catch(err => {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            return next(err);
+            res.status(500).json({
+                message: "Deleting product failed."
+            });
+            // const error = new Error(err);
+            // error.httpStatusCode = 500;
+            // return next(err);
         });
 };
 
